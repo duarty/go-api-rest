c7b3446e5ec3a2bce826ddbbbc88c180e29187d0
Duarte <duartydev@gmail.com>
2024-05-21 21:23:39 -0400

    feat: first request sucess

--- main.go
+++ main.go
@@ -1,17 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"encoding/json"
 	"fmt"
 	"goapirest/configs"
 	"io"
+	"log"
 	"net/http"
-	"strings"
 )
 
-type GymRequest struct{
-	IncludedTypes []string `json:"includedTypes"`
-	MaxResultCount  int `json:"maxResultCount"`
+type GymRequest struct {
+	IncludedTypes       []string             `json:"includedTypes"`
+	MaxResultCount      uint8                  `json:"maxResultCount"`
 	LocationRestriction *LocationRestriction `json:"locationRestriction"`
 }
 
@@ -26,46 +27,92 @@ type Circle struct {
 
 type Center struct {
 	Longitude float64 `json:"longitude"`
-	Latitude float64 `json:"latitude"`
+	Latitude  float64 `json:"latitude"`
+}
+
+type GymResponse struct {
+	Places *[]Place `json:"places"`
+}
+
+type Place struct {
+	DisplayName DisplayName `json:"displayName"`
+}
+
+type DisplayName struct {
+	Text         string `json:"text"`
+	LanguageCode string `json:"languageCode"`
 }
 
 func main() {
-	_, err := configs.LoadEnv(".env")
+	config, err := configs.LoadEnv(".env")
 	if err != nil {
 		panic(fmt.Errorf("fatal error config file: %w", err))
 	}
 
-	var gymRequestData = &GymRequest{
-		IncludedTypes: []string{"gym"},
-		MaxResultCount: 1,
-		LocationRestriction: &LocationRestriction{
-			Circle: &Circle{
-				Center: &Center{
-					Longitude: 2.814704860187976,
-					Latitude: -60.72387482874884,
+	// data := &GymRequest{
+	// 	IncludedTypes:  []string{"gym"},
+	// 	MaxResultCount: 1,
+	// 	LocationRestriction: &LocationRestriction{
+	// 		Circle: &Circle{
+	// 			Center: &Center{
+	// 				Longitude: 2.814704860187976,
+	// 				Latitude:  -60.72387482874884,
+	// 			},
+	// 			Radius: 1000.0,
+	// 		},
+	// 	},
+	// }
+	data := map[string]interface{}{
+		"includedTypes": []string{"gym"},
+		"maxResultCount": 1,
+		"locationRestriction": map[string]interface{}{
+			"circle": map[string]interface{}{
+				"center": map[string]float64{
+					"latitude": 2.8135529076686567,
+					"longitude": -60.67046458758208,
 				},
-				Radius: 50.0,
+				"radius": 10,
 			},
 		},
 	}
 
-	gymRequestDataJson, err := json.Marshal(gymRequestData)
+	jsonData, err := json.Marshal(data)
 	if err != nil {
-		panic(err)
+		log.Fatalf("Erro codificando dados JSON: %v", err)
 	}
-	body := strings.NewReader(string(gymRequestDataJson))
-	req, err := http.Post("https://places.googleapis.com/v1/places:searchNearby", "application/json",body)
+
+	body := bytes.NewBuffer(jsonData)
+
+	URL := config.GoogleMapsApiEndPoint + config.GoogleMapsApiPlaces
+
+	req, err := http.NewRequest("POST", URL, body)
 	if err != nil {
 		panic(err)
 	}
 
-	defer req.Body.Close()
+	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("X-Goog-Api-Key", config.GoogleMapsApiKey)
+	req.Header.Set("X-Goog-FieldMask", "places.displayName")
 
-	responseBody, err := io.ReadAll(req.Body)
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		panic(err)
+	}
+	defer resp.Body.Close()
+
+	responseBody, err := io.ReadAll(resp.Body)
 	if err != nil {
 		fmt.Println("Error reading response:", err)
 		return
 	}
-	fmt.Println("Response Body:", string(responseBody))
+
+	var gymResponse GymResponse
+	err = json.Unmarshal(responseBody, &gymResponse)
+	if err != nil {
+		fmt.Println("Error unmarshaling response:", err)
+		return
+	}
+
+	fmt.Printf("Deserialized Response: %+v\n", gymResponse.Places)
 
 }
