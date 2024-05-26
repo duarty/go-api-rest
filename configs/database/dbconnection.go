10b3c9ac5801980e62aacdccc3ab4a42124f8ded
Duarte <duartydev@gmail.com>
2024-05-25 20:07:20 -0400

    feat(database): update database connection function

--- configs/database/dbconnection.go
+++ configs/database/dbconnection.go
@@ -3,14 +3,17 @@ package database
 import (
 	"fmt"
 	"goapirest/configs"
-	"os"
 
 	"github.com/jackc/pgx"
 )
 
 
-func DBConnection () {
-	config, _ := configs.LoadEnv(".env")
+func DBConnection () (*pgx.Conn, error) {
+	config, err := configs.LoadEnv(".env")
+	if err != nil {
+		panic(fmt.Errorf("fatal error config file: %w", err))
+	}
+
 	postgresConfig := pgx.ConnConfig{
 		Host: config.DBHost,
 		Port: config.DBPort,
@@ -18,30 +21,13 @@ func DBConnection () {
 		User: config.DBUser,
 		Password: config.DBPassword,
 	}
+
 	conn, err := pgx.Connect(postgresConfig)
 	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to connect to database: %v\n", err)
-		os.Exit(1)
+		panic(fmt.Errorf("Unable to connect to database: %v", err))
 	}
-	// ctx := context.Background()
-	// ping := conn.Ping(ctx)
-
-	// select {
-	// case <-time.After(3 * time.Second):
-	// 	ping.Error()
-	// case <-ctx.Done():
-	// 	fmt.Println("Conexão com o banco de dados realizada com sucesso!")
-	// }
-
-	// defer conn.Close()
 	fmt.Print(conn.PID())
-	// var name string
-	// var weight int64
-	// err = conn.QueryRow(context.Background(), "select name, weight from widgets where id=$1", 42).Scan(&name, &weight)
-	// if err != nil {
-	// 	fmt.Fprintf(os.Stderr, "QueryRow failed: %v\n", err)
-	// 	os.Exit(1)
-	// }
 
-	// fmt.Println(name, weight)
+	return conn, nil
+
 }
