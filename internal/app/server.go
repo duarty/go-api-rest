0cd11fad46feb39249a7e77398cfb664063561ec
Duarte <duartydev@gmail.com>
2024-05-29 20:32:33 -0400

    feat(server): add application server

--- internal/app/server.go
+++ internal/app/server.go
@@ -1,1 +1,17 @@
 package app
+
+import (
+	"io"
+	"log"
+	"net/http"
+)
+
+
+func Server() {
+	helloHandler := func(w http.ResponseWriter, req *http.Request) {
+		io.WriteString(w, "Hello, world!\n")
+	}
+
+	http.HandleFunc("/hello", helloHandler)
+	log.Fatal(http.ListenAndServe(":8080", nil))
+}
