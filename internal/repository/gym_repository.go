5b8e500d46eca1e2031fab96149123555cd61736
Duarte <duartydev@gmail.com>
2024-05-30 21:40:28 -0400

    fix(gym): fix repository

--- internal/repository/gym_repository.go
+++ internal/repository/gym_repository.go
@@ -8,28 +8,27 @@ import (
 	"github.com/jackc/pgx"
 )
 
-// type GymRepository interface {
-//     CreateGym(gym *model.Gym) error
-// }
+type GymRepository interface {
+	CreateGym(gym *model.Gym) error
+}
 
 type GymRepo struct {
-    db *pgx.Conn
+	db *pgx.Conn
 }
 
 func NewGymRepo(db *pgx.Conn) *GymRepo {
-    return &GymRepo{db: db}
+	return &GymRepo{db: db}
 }
 
 func (r *GymRepo) CreateGym(gym *model.Gym) error {
-	pstmt, err := r.db.Prepare("createGym", "INSERT INTO gyms (id, name, address, placeID, longitude, latitude) VALUES ($1, $2, $3, $4, $5, $6)")
+	pstmt, err := r.db.Prepare( "createGym", "INSERT INTO gyms (id, name, address, placeID, longitude, latitude) VALUES ($1, $2, $3, $4, $5, $6)")
 	if err != nil {
-        log.Fatalf("Prepare failed: %v\n", err)
-    }
+		log.Fatalf("Prepare failed: %v\n", err)
+	}
 
-	_, err =  r.db.Exec(pstmt.SQL, gym.ID, gym.Name, gym.Address, gym.PlaceID, gym.Longitude, gym.Latitude)
+	_, err = r.db.Exec(pstmt.SQL, gym.ID, gym.Name, gym.Address, gym.PlaceID, gym.Longitude, gym.Latitude)
 	if err != nil {
-		return fmt.Errorf("unable to insert user: %w", err)
+		return fmt.Errorf("unable to insert gym: %w", err)
 	}
 	return nil
 }
-
