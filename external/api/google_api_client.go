d1dd2896683d60946853e8607f1094f427ee5251
Duarte <duartydev@gmail.com>
2024-06-01 15:20:08 -0400

    fix(external): change wrong return

--- external/api/google_api_client.go
+++ external/api/google_api_client.go
@@ -3,3 +3,11 @@ package api
 type GoogleMapsClient interface {
     GetGymsFromCoordinates(lat, lng float64) (string, error)
 }
+
+type googleMapsClient struct {
+    apiKey string
+}
+
+func NewGoogleMapsClient(apiKey string) *googleMapsClient {
+    return &googleMapsClient{apiKey: apiKey}
+}
