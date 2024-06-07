f5740defd6a5817c0fd0d339a4f09fde2369291b
Duarte <duartydev@gmail.com>
2024-06-07 14:27:14 -0400

    fix(database/gym_repo): change sql.DB --> pgx.Conn

--- internal/infra/database/gym_repository.go
+++ internal/infra/database/gym_repository.go
@@ -1,30 +1,29 @@
 package database
 
 import (
-	"database/sql"
 	"fmt"
 	"goapirest/internal/entity"
 	"log"
+
+	"github.com/jackc/pgx"
 )
 
 type GymRepository struct {
-	Db *sql.DB
+	Db *pgx.Conn
 }
 
-func NewGymRepository(db *sql.DB) *GymRepository {
+func NewGymRepository(db *pgx.Conn) *GymRepository {
 	return &GymRepository{Db: db}
 }
 
 func (r *GymRepository) Save(gym *entity.Gym) error {
-
-	pstmt, err := r.Db.Prepare("insertUser", "INSERT INTO gyms (name, address, placeID, longitude, latitude) VALUES ($1, $2, $3, $4, $5)")
+	pstmt, err := r.Db.Prepare( "createGym", "INSERT INTO gyms (name, address, placeID, longitude, latitude) VALUES ($1, $2, $3, $4, $5)")
 	if err != nil {
-        log.Fatalf("Prepare failed: %v\n", err)
-    }
-
+		log.Fatalf("Prepare failed: %v\n", err)
+	}
 	_, err = r.Db.Exec(pstmt.SQL, gym.Name, gym.Address, gym.PlaceID, gym.Longitude, gym.Latitude)
 	if err != nil {
-		return fmt.Errorf("unable to insert user: %w", err)
+		return fmt.Errorf("unable to insert gym: %w", err)
 	}
 	return nil
 }
