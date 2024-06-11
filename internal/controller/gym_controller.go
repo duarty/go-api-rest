c8ce213845ce71ccbbd43c84098920906234da1e
SeuNome <duartydev@gmail.com>
2024-06-11 14:07:25 -0400

    chore(controller/gy): update gym controller

--- internal/controller/gym_controller.go
+++ internal/controller/gym_controller.go
@@ -1,19 +1,24 @@
 package controller
 
+//chamar primeiro GetGymController para ver se está no raio de alguma academia
+//Se tiver no raio, retornar as academias desse raio para o usuário escolher a que gostaria de fazer check-in
+//se não, s
+
 import (
-	"goapirest/internal/dto"
 	"goapirest/internal/usecase"
 	"net/http"
 )
 
 type GymController struct {
-	userUseCase *usecase.CreateGymUseCase
+	gymUseCase *usecase.GymUseCase
 }
 
-func NewGymController(u *usecase.CreateGymUseCase) *GymController {
-	return &GymController{userUseCase: u}
+func NewGymController(u *usecase.GymUseCase) *GymController {
+	return &GymController{gymUseCase: u}
 }
 
-func (c *GymController) Create(r http.Request, w http.ResponseWriter) {
-	var gymDto dto.GymInputDTO
+func (c *GymController) CreateGymController(w http.ResponseWriter, r *http.Request) {
+
+	// var gymDto dto.GymInputDTO
+	// c.gymUseCase.Execute()
 }
