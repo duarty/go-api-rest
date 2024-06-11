396f1f7ddba94f8786007a325a7d4e018cc4c79e
SeuNome <duartydev@gmail.com>
2024-06-11 17:40:05 -0400

    feat(controller/gym): add POST method on create gym

--- internal/controller/gym_controller.go
+++ internal/controller/gym_controller.go
@@ -5,7 +5,11 @@ package controller
 //se não, s
 
 import (
+	"encoding/json"
+	"fmt"
+	"goapirest/internal/dto"
 	"goapirest/internal/usecase"
+	"io"
 	"net/http"
 )
 
@@ -18,7 +22,44 @@ func NewGymController(u *usecase.GymUseCase) *GymController {
 }
 
 func (c *GymController) CreateGymController(w http.ResponseWriter, r *http.Request) {
+	// data := map[string]interface{}{
+	// 	"includedTypes": []string{config.IncludedTypes},
+	// 	"maxResultCount": 1,
+	// 	"locationRestriction": map[string]interface{}{
+	// 		"circle": map[string]interface{}{
+	// 			"center": map[string]float64{
+	// 				"latitude": 2.8135529076686567,
+	// 				"longitude": -60.67046458758208,
+	// 			},
+	// 			"radius": config.NearbySearchRadius,
+	// 		},
+	// 	},
+	// }
 
+	// jsonData, err := json.Marshal(data)
+	// if err != nil {
+	// 	log.Fatalf("Erro codificando dados JSON: %v", err)
+	// }
+
+	// body := bytes.NewBuffer(jsonData)
+	if r.Method == "POST" {
+		defer r.Body.Close()
+		body, err := io.ReadAll(r.Body)
+		if err != nil {
+			panic(err)
+		}
+		var format dto.CheckInDTO
+		err = json.Unmarshal(body, &format)
+		if err != nil {
+			panic(err)
+		}
+
+		fmt.Print(format.Latitude)
+	}
+
+	// if r.Method == "GET" {
+	// 	io.Writer()
+	// }
 	// var gymDto dto.GymInputDTO
 	// c.gymUseCase.Execute()
 }
